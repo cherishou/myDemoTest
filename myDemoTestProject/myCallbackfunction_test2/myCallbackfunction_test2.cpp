8b0b43bb86e66440f57d61804d9506e7b4c0ff28
DESKTOP-UN6O5U8\Cherish <cherishou@foxmail.com>
2020-08-13 23:07:30 +0800

    demo upload

--- myDemoTestProject/myCallbackfunction_test2/myCallbackfunction_test2.cpp
+++ myDemoTestProject/myCallbackfunction_test2/myCallbackfunction_test2.cpp
@@ -2,10 +2,76 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <functional>
+#include <set>
+#include <memory>
+
+class Client {
+public:
+	void set_cbfunc(std::function<void()> f) {
+		m_cbFunc = std::move(f);
+	}
+
+	void callback() {
+		std::cout << "client callback id: " << id << std::endl;
+		m_cbFunc();
+	}
+
+	// 比较函数, 仅仅是为了区分确定的客户端, 方便在容器中比较
+	inline bool operator<(const Client &client) {
+		return bool(this < &client);
+	}
+
+	inline int getId() const {
+		return id;
+	}
+
+	inline void setId(int id_) {
+		id = id_;
+	}
+
+private:
+	int id;
+	std::function<void()> m_cbFunc;
+};
+
+class Server {
+public:
+	void callback(std::shared_ptr<Client> &p) {
+		std::cout << "Server callback, id: " << p->getId() << std::endl;
+		m_pClients.erase(p);
+	}
+
+	void start() {
+		std::cout << "Before CallBack, Client num: " << m_pClients.size() << std::endl;
+		for (auto &p : m_pClients) {
+			std::cout << "client id: " << p->getId() << std::endl;
+			p->callback();
+		}
+		std::cout << "After CallBackClient num: " << m_pClients.size() << std::endl;
+	}
+
+	std::set<std::shared_ptr<Client>> m_pClients;
+};
+
+
+
+
+
 
 
 int main()
 {
+	Server server;
+	for (int i = 0; i < 5; ++i) {
+		auto it = std::make_shared<Client>();
+		it->setId(i);
+		it->set_cbfunc(std::bind(&Server::callback, &server, it));
+		server.m_pClients.insert(it);
+	}
+	server.start();
+
     return 0;
 }
 
